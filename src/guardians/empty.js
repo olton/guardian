7485d228486a2e56a18ed50135219d489e8dfc51
Serhii Pimenov <serhii@pimenov.com.ua>
2024-07-19 12:38:21 +0300

    add empty

---
+++ src/guardians/empty.js
@@ -0,0 +1,14 @@
+import {GuardianError} from "../error";
+
+const GUARD_EMPTY_MESSAGE = 'VAL must be a string or array with length 0'
+
+export default (errorMessage = GUARD_EMPTY_MESSAGE) => {
+    return function (input) {
+        const msg = errorMessage.replace(/VAL/g, input).replace(/N/g, length)
+        const check = (typeof input === "string" || Array.isArray(input)) && input.length === 0
+        if (!check) {
+            return new GuardianError( msg,"string", input )
+        }
+        return input
+    }
+}
