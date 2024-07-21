ae6e96dc9003fae3e87cb86bf7dd797a922575ba
olton <serhii@pimenov.com.ua>
2024-07-21 20:20:25 +0300

    upd object

--- src/info.js
+++ src/info.js
@@ -1,5 +1,5 @@
-const version = "0.2.0"
-const build_time = "21.07.2024, 20:13:01"
+const version = "0.3.0"
+const build_time = "21.07.2024, 20:20:20"
 
 export default () => {
     console.info(`%c GUARDIAN %c v${version} %c ${build_time} `, "color: pink; font-weight: bold; background: #2b1700", "color: white; background: darkgreen", "color: white; background: #0080fe;")
