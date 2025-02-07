3e71c90ac9323539e0cc82f3d451c018c6a617cb
Serhii Pimenov <serhii@pimenov.com.ua>
2025-02-07 03:04:00 +0200

    upd info

--- src/info/info.js
+++ src/info/info.js
@@ -2,5 +2,5 @@ const version = "__VERSION__"
 const build_time = "__BUILD_TIME__"
 
 export default () => {
-    console.info(`%c GUARDIAN %c v${version} %c ${build_time} `, "color: pink; font-weight: bold; background: #2b1700", "color: white; background: darkgreen", "color: white; background: #0080fe;")
+    console.info(`%c Guardian %c v${version} %c ${build_time} `, "color: pink; font-weight: bold; background: #2b1700", "color: white; background: darkgreen", "color: white; background: #0080fe;")
 }
